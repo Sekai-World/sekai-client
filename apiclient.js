494ae5d7382058da77209e4497c69143fe16d332
DNARoma <tangpei506@gmail.com>
2020-12-26 17:23:45 +0100

    feat: expose sekai api as service

--- apiclient.js
+++ apiclient.js
@@ -90,3 +90,214 @@ module.exports.initialHeader = initialHeader;
 
 module.exports.decrypt = decrypt;
 module.exports.encrypt = encrypt;
+
+module.exports.APIClient = class APIClient {
+  constructor(logger) {
+    if (!logger) {
+      throw new Error("logger is missing.");
+    }
+    this.axios = axios.default.create({
+      baseURL,
+      transformRequest: [
+        (data, headers) => {
+          headers["x-request-id"] = uuidV4();
+          headers["content-type"] = "application/octet-stream";
+          return data;
+        },
+      ],
+      responseType: "arraybuffer",
+    });
+
+    this.headers = Object.assign({}, initialHeader);
+    this.logger = logger;
+
+    this.isRateLimited = false;
+
+    this.axios.interceptors.response.use(
+      (res) => {
+        if (res.headers["x-session-token"])
+          this.headers["x-session-token"] = res.headers["x-session-token"];
+
+        if (res.data.length) res.data = decrypt(Buffer.from(res.data));
+        return res;
+      },
+      async (err) => {
+        // console.log(err);
+        if (err.response.data.length) {
+          err.response.data = decrypt(Buffer.from(err.response.data));
+        }
+        this.logger.error(err.response.status, err.response.data);
+        // const req = err.config;
+        if (err.response.status === 429) {
+          // hit rate limit, sleep for a while
+          this.logger.warn("rate limit hit, sleep for 60s");
+          this.isRateLimited = true;
+          await delay(60000);
+          this.isRateLimited = false;
+        } else if (err.response.status === 426) {
+          this.logger.warn("should update version");
+          await this.checkVersions();
+        }
+
+        // console.log(err.request)
+
+        throw err;
+      }
+    );
+  }
+
+  set account(data) {
+    this._account = data;
+    // console.log(this._account);
+  }
+
+  get account() {
+    return this._account;
+  }
+
+  get userInfo() {
+    return this._userInfo;
+  }
+
+  async callAPI(endpoint, method = "get", body) {
+    if (this.isRateLimited) {
+      throw new Error("rate limit hit, cooling down.");
+    }
+    const { data } = await this.axios({
+      url: endpoint,
+      method,
+      headers: this.headers,
+      data: ["post", "put", "patch"].includes(method) ? encrypt(body) : null,
+    });
+
+    return data;
+  }
+
+  async registerAccount() {
+    // this.logger.info("create a new account");
+    return await this.callAPI("/user", "post", {
+      platform: "iOS",
+      deviceModel: "iPad6,11",
+      operatingSystem: "iOS 13.5",
+    });
+  }
+
+  async login() {
+    this.logger.info("simulate login process");
+    this.logger.debug("do auth");
+    const { userId, credential } = this._account;
+    const {
+      sessionToken,
+      appVersion,
+      dataVersion,
+      assetVersion,
+    } = await this.callAPI(
+      `/user/${userId}/auth?refreshUpdatedResources=False`,
+      "put",
+      {
+        credential,
+      }
+    );
+    // this.logger.info(`user ${userId} logged in`);
+    this.headers["x-session-token"] = sessionToken;
+    this.headers["x-app-version"] = appVersion;
+    this.headers["x-data-version"] = dataVersion;
+    this.headers["x-asset-version"] = assetVersion;
+    this.logger.info(
+      `login app version ${appVersion} master version ${dataVersion} asset version ${assetVersion}`
+    );
+
+    this.logger.debug("get system");
+    await this.callAPI("/system");
+
+    // const { userId } = this.account;
+    this.logger.debug("get suite user");
+    const userInfo = await this.callAPI(`/suite/user/${userId}`);
+
+    // check tutorial status
+    const { userTutorial } = userInfo;
+    if (userTutorial.tutorialStatus === "start") {
+      this.logger.warn("tutorial is at start, set username first");
+      await this.callAPI(`/user/${userId}/tutorial`, "patch", {
+        tutorialStatus: "opening_1",
+      });
+      await this.callAPI(`/user/${userId}`, "patch", {
+        userGamedata: {
+          name: "\u30bb\u30ab\u30a4\u306e\u4f4f\u4eba",
+        },
+      });
+      userTutorial.tutorialStatus = "opening_1";
+    }
+    // skip tutorial
+    if (userTutorial.tutorialStatus !== "end") {
+      this.logger.debug("rolling tutorial");
+      const steps = [
+        "opening_1",
+        "gameplay",
+        "opening_2",
+        "unit_select",
+        "idol_opening",
+        "summary",
+        "end",
+      ];
+      for (let status of steps.slice(
+        steps.indexOf(userTutorial.tutorialStatus) + 1
+      )) {
+        await this.callAPI(`/user/${userId}/tutorial`, "patch", {
+          tutorialStatus: status,
+        });
+      }
+    }
+
+    this.logger.debug("refresh home login_bonus");
+    await this.callAPI(`/user/${userId}/home/refresh`, "put", {
+      refreshableTypes: ["login_bonus"],
+    });
+
+    this._userInfo = userInfo;
+    return userInfo;
+  }
+
+  async getUserProfile(userId) {
+    return await this.callAPI(`/user/${userId}/profile`);
+  }
+
+  async checkVersions() {
+    const res = {
+      isMaintenance: false,
+      isNewVersion: false,
+    };
+    const { appVersions } = await this.callAPI("/system");
+    this.logger.debug(appVersions);
+    let currentVersion = appVersions.find(
+      (appVer) =>
+        appVer.appVersion === this.headers["x-app-version"] &&
+        appVer.appVersionStatus === "available"
+    );
+
+    if (!currentVersion) {
+      // check latest version
+      currentVersion = appVersions[appVersions.length - 1];
+      if (currentVersion.appVersionStatus === "maintence") {
+        res.isMaintenance = true;
+      } else if (currentVersion.appVersionStatus === "available") {
+        res.isNewVersion = true;
+      }
+    } else {
+      res.isNewVersion =
+        this.headers["x-data-version"] !== currentVersion.dataVersion ||
+        this.headers["x-asset-version"] !== currentVersion.assetVersion ||
+        this.headers["x-app-version"] !== currentVersion.appVersion;
+    }
+    if (res.isNewVersion) {
+      this.headers["x-app-version"] = currentVersion.appVersion;
+      this.headers["x-asset-version"] = currentVersion.assetVersion;
+      this.headers["x-data-version"] = currentVersion.dataVersion;
+      this.logger.info(
+        `get new version, app version ${currentVersion.appVersion} master version ${currentVersion.dataVersion} asset version ${currentVersion.assetVersion}`
+      );
+    }
+
+    return res;
+  }
+};
