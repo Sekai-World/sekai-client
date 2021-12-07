24a437dd60f10a23a27ab3d4682e255c59a45463
DNARoma <tangpei506@gmail.com>
2021-12-07 19:23:31 +0100

    feat: queryAsApi support multiple regions

--- queryAsApi.js
+++ queryAsApi.js
@@ -16,36 +16,45 @@ const logger = log4js.getLogger("query-as-api");
 logger.level = "info";
 
 const app = new Koa();
-const router = new Router();
 
 import jayson from "jayson/promise";
 
-const client = new jayson.Client.http({
-  port: process.env.SERVER_PORT || 3939, // change port to use different server
-});
+const regionalClientMap = {
+  jp: process.env.SERVER_JP_PORT
+    ? new jayson.Client.http({
+        port: process.env.SERVER_JP_PORT,
+      })
+    : null,
+  tw: process.env.SERVER_TW_PORT
+    ? new jayson.Client.http({
+        port: process.env.SERVER_TW_PORT,
+      })
+    : null,
+  en: process.env.SERVER_EN_PORT
+    ? new jayson.Client.http({
+        port: process.env.SERVER_EN_PORT,
+      })
+    : null,
+};
 
 async function bootstrap() {
-  await clientRequest(client, "init", [region]);
-  await clientRequest(client, "checkVersions", []);
-  await clientRequest(client, "login", []);
+  for (let key in regionalClientMap) {
+    const client = regionalClientMap[key];
+    if (client) {
+      await clientRequest(client, "init", [region]);
+      await clientRequest(client, "checkVersions", []);
+      await clientRequest(client, "login", []);
+    }
+  }
 }
 
-const reLoginJob = new CronJob(
-  "0 0 4 * * *",
-  bootstrap,
-  null,
-  false,
-  "Asia/Tokyo"
-);
-
-router.get("/health", async (ctx, next) => {
-  const isHealthy = true; // apiClientPool.some((apiClient) => !!apiClient.account);
-  ctx.body = {
-    status: isHealthy ? "success" : "error",
-  };
-  ctx.status = isHealthy ? 200 : 500;
-  next();
-});
+// const reLoginJob = new CronJob(
+//   "0 0 4 * * *",
+//   bootstrap,
+//   null,
+//   false,
+//   "Asia/Tokyo"
+// );
 
 const protectedRoute = async (ctx, next) => {
   if (ctx.headers["x-api-token"] !== process.env.API_TOKEN) {
@@ -61,9 +70,37 @@ const protectedRoute = async (ctx, next) => {
   }
 };
 
-router.get("/user/:id/profile", protectedRoute, async (ctx, next) => {
+const getRegionalClient = async (ctx, next) => {
+  const client = regionalClientMap[ctx.params.region];
+  if (!client) {
+    ctx.body = {
+      status: "error",
+      message: "No such region.",
+    };
+    ctx.status = 400;
+    return;
+  }
+  ctx.client = client;
+  return next();
+};
+
+const router = new Router();
+
+router.get("/health", async (ctx, next) => {
+  const regions = Object.keys(regionalClientMap);
+  const isHealthy = regions.some((region) => !!regionalClientMap[region]);
+  ctx.body = {
+    status: isHealthy ? "success" : "error",
+  };
+  ctx.status = isHealthy ? 200 : 500;
+  return next();
+});
+
+const userRouter = new Router();
+
+userRouter.get("/:id/profile", protectedRoute, async (ctx, next) => {
   try {
-    const userData = await clientRequest(client, "callAPI", [
+    const userData = await clientRequest(ctx.client, "callAPI", [
       `/user/${ctx.params.id}/profile`,
     ]);
 
@@ -72,7 +109,7 @@ router.get("/user/:id/profile", protectedRoute, async (ctx, next) => {
       data: userData,
     };
   } catch (error) {
-    logger.error(error.response);
+    logger.error(error);
     ctx.body = {
       status: "error",
       message: "check your input.",
@@ -83,13 +120,13 @@ router.get("/user/:id/profile", protectedRoute, async (ctx, next) => {
   return next();
 });
 
-router.get(
-  "/user/:id/event/:eventId/ranking",
+userRouter.get(
+  "/:id/event/:eventId/ranking",
   protectedRoute,
   async (ctx, next) => {
     try {
-      const account = await clientRequest(client, "account", []);
-      const eventRanking = await clientRequest(client, "callAPI", [
+      const account = await clientRequest(ctx.client, "account", []);
+      const eventRanking = await clientRequest(ctx.client, "callAPI", [
         `/user/${account.userId}/event/${ctx.params.eventId}/ranking?targetUserId=${ctx.params.id}`,
       ]);
 
@@ -98,7 +135,7 @@ router.get(
         data: eventRanking,
       };
     } catch (error) {
-      logger.error(error.response);
+      logger.error(error);
       ctx.body = {
         status: "error",
         message: "check your input.",
@@ -109,16 +146,25 @@ router.get(
   }
 );
 
-router.post("/refresh", protectedRoute, async (ctx, next) => {
-  await clientRequest(client, "relogin", []);
+router
+  .use("/:region/user", protectedRoute, getRegionalClient, userRouter.routes())
+  .use(userRouter.allowedMethods());
 
-  ctx.body = {
-    status: "success",
-  };
+router.post(
+  "/:region/refresh",
+  protectedRoute,
+  getRegionalClient,
+  async (ctx, next) => {
+    await clientRequest(ctx.client, "relogin", []);
 
-  return next();
-});
+    ctx.body = {
+      status: "success",
+    };
+
+    return next();
+  }
+);
 
 app.use(router.routes()).use(router.allowedMethods());
 app.listen(process.env.PORT || 9393, "localhost");
-bootstrap().then(() => reLoginJob.start());
+bootstrap();
