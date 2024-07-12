59b6b331f73301d746df7a352790ff283a202f2e
DNARoma <dev@sekai.best>
2024-07-12 09:18:57 +0100

    preserve ids in musics and cards

--- check_update.py
+++ check_update.py
@@ -364,7 +364,7 @@ def refresh_version():
         try:
             id_key = None
             if any(x in key for x in [
-                    "event", "gacha", "virtual", "cheerfulCarnival", "tips"
+                    "event", "gacha", "virtual", "cheerfulCarnival", "tips", "musics", "cards"
             ]) and (pjsk_region not in ["tw", "kr"] and key != "eventCards"):
                 id_key = "id"
             elif (pjsk_region not in ["tw", "kr"] and key != "eventCards"):
